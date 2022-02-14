dda908a52aab20947eeaadfd2d7de5f306012853
Daniel <dkilcullen25@gmail.com>
2022-02-14 13:01:31 -0600

    post trip

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,12 @@
 class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   
-  # Add your routes here
+  # return all trips
   get '/trips' do
     Trip.all.to_json
   end
 
-  #placeholder
+  #placeholder- get dummy user and trips and segments
   get '/users' do
     User.first.to_json(
       include: {
@@ -15,18 +15,22 @@ class ApplicationController < Sinatra::Base
     )
   end
   
-  #returns trip with segments
+  #returns all user's trips and segments
   get "/trips/:id" do
     user_trips = Trip.all.filter{|trip| trip.user_id == params[:id].to_i }
     user_trips.to_json include: :segments
   end
 
+  # posts a trip and segments to a user
   post '/trips' do
     new_trip = Trip.create(user_id: params[:user_id], description: params[:tripName])
     
     params[:segments].each do |segment|
       Segment.create(from: segment[:from], to: segment[:to], date: segment[:date], trip_id: new_trip.id)
     end
+
+    #return all user's trips after post
+    new_trip.to_json(include: :segments)
   end
 
 end
